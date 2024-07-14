ff64c20e9dcb1f41752238d0a1186c428ea4e5eb
Muhammad Azizi <azizi@hotmail.com.my>
2024-07-14 08:36:37 +0800

    Created a global custom Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="input-container">
+    <!-- <img v-if="showIcon" class="input-logo" src="../assets/logo.png" alt=""> -->
+    <img v-if="icon" class="input-logo" :src="require(`../assets/${icon}`)" alt="">
+    <input class="input-element" :type="inputType" v-model="inputValue" :placeholder="placeholder"
+      @focus="inputIsFocus = true" @blur="inputIsFocus = false" @input="emit('update:value', inputValue)" ref="inputEl">
+    <Transition name="appear">
+      <img v-if="inputValue && !disableClear" class="input-clear" @click="clearClicked"
+        src="../assets/input/clear.png" alt="">
+    </Transition>
+    <div class="line-container">
+      <div class="line"></div>
+      <div class="line line-focus" :style="{ maxWidth: inputIsFocus ? '100%' : '0' }"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/* eslint-disable */
+import { ref, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  showIcon: { type: Boolean, default: false },
+  value: String,
+  icon: String,
+  disableClear: { type: Boolean, default: false },
+  inputType: { type: String, default: 'text' },
+  placeholder: { type: String, default: 'Custom Input' }
+});
+
+const emit = defineEmits([
+  'update:value'
+]);
+
+//#region Data Section
+const inputValue = ref('');
+const inputIsFocus = ref(false);
+const inputEl = ref(null);
+//#endregion Data Section
+
+//#region Methods
+const clearClicked = () => {
+  inputValue.value = '';
+  inputEl.value.focus();
+
+  emit('update:value', inputValue.value);
+}
+//#endregion Methods
+</script>
+
+<style scoped>
+.input-container {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  position: relative;
+}
+.input-logo {
+  height: 20px;
+}
+.input-clear {
+  height: 15px;
+  cursor: pointer;
+}
+.input-element {
+  width: 100%;
+  border: none;
+  margin: 0 5px;
+  padding: 5px;
+}
+.input-element:focus {
+  outline: none;
+}
+.line-container {
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.line {
+  height: 1px;
+  background-color: gray;
+  width: 100%;
+  margin-top: 5px;
+  position: absolute;
+}
+.line-focus {
+  height: 2px;
+  width: 100%;
+  background-color: rgb(68,188,98);
+  transition: 0.3s;
+}
+
+.appear-enter-active, .appear-leave-active {
+  transition: 0.3s;
+}
+.appear-enter-from, .appear-leave-to {
+  transform: scale(0.5);
+  opacity: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+/* eslint-disable */
 
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+const app = createApp(App);
+
+//#region Global component section
+import Input from './components/Input.vue';
+
+app.component('Input', Input);
+//#endregion Global component section
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
