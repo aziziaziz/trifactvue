cd43df89f92c6a97231a116bb1855414e7f09fe1
Muhammad Azizi <azizi@hotmail.com.my>
2024-07-14 10:00:31 +0800

    First part of the button designing

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    
-  </div>
+  <button class="button-container">
+    <slot>
+      Testing
+    </slot>
+  </button>
 </template>
 
 <script setup>
@@ -9,5 +11,19 @@ import {} from 'vue';
 </script>
 
 <style scoped>
-
+.button-container {
+  width: 100%;
+  padding: 10px;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  background-color: rgba(68,188,98,0.6);
+  transition: 0.3s;
+}
+.button-container:focus {
+  outline-color: rgb(43, 121, 63);
+}
+.button-container:active {
+  transform: scale(0.9);
+}
 </style>
