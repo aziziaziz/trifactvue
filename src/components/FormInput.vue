1e53586bd5255935b245ba3b3ce6dd79189ab814
Muhammad Azizi <azizi@hotmail.com.my>
2024-07-24 15:05:34 +0800

    Handle for the input form to have 2 way binding on the input value

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,16 +1,30 @@
 <template>
 	<div class="input-main">
 		<div class="input-label">{{ props.placeholder }}</div>
-		<input class="input-box" type="text">
+		<input class="input-box" type="text" v-model="inputValue" @input="userInput">
 	</div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, defineEmits } from 'vue';
 
 const props = defineProps({
 	placeholder: { type: String, default: 'Label' }, // Use the name the label/placeholder
+	value: String, // Use for the value and it is binding to inputValue
 });
+const emit = defineEmits([
+	'update:value'
+])
+
+//#region Data
+const inputValue = ref(''); // Private value binded to the input element
+//#endregion Data
+
+//#region Method
+const userInput = () => {
+	emit('update:value', inputValue.value);
+}
+//#endregion Method
 </script>
 
 <style scoped>
