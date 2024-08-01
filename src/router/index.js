c02b961860eb8068fc4a590bab7e8530ad0afaf0
Muhammad Azizi <azizi@hotmail.com.my>
2024-08-01 14:03:24 +0800

    Added display name for each of the page

--- src/router/index.js
+++ src/router/index.js
@@ -15,16 +15,19 @@ const routes = [
       {
         path: '',
         name: 'Home',
+        meta: { displayName: 'Current Location' },
         component: () => import('../views/home/Home.vue')
       },
       {
         path: 'SpaceReq',
         name: 'Space Requirements [Phase 1]',
+        meta: { displayName: 'Space Requirements' },
         component: () => import('../views/phase1/SpaceRequirements.vue')
       },
       {
         path: 'OfficeCustom',
         name: 'Office Customization [Phase 1]',
+        meta: { displayName: 'Office Customization' },
         component: () => import('../views/phase1/OfficeCustomization.vue')
       }
     ]
