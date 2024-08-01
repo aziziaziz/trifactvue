47dc59fbbead11497489babced69464d2ec06b33
Muhammad Azizi <azizi@hotmail.com.my>
2024-08-01 14:20:16 +0800

    Completed the home page to add location

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,134 @@
 <template>
-  <div class="home-main">
-    WELCOME BACK {{ username }}!
+  <div class="home-container">
+    <Button theme="submit" class="add-space-button" @click="addLocationClicked">Add Location</Button>
+    <Input placeholder="Search Space" />
+    <div v-if="allLocations.length > 0" class="space-container">
+      <div class="space-details" v-for="(space, spaceInd) in allLocations" :key="spaceInd" @click="locationDetailsClicked(spaceInd)">
+        <div class="space-title">{{ store.state.currentLocation ? (store.state.currentLocation.location == space.location ? '✓' : '') : '' }} {{ space.location }}</div>
+        <div class="space-description-container">{{ space.description.map(d => `${d.count} ${d.name.value} (${d.totalSpace} ${space.unit})`).join('; ') }}</div>
+      </div>
+    </div>
+    <div v-else-if="locationLoading">Loading Locations</div>
+
+
+    <Popup :show="showAddLocationPopup">
+      <template v-slot:header>Add Location</template>
+      <template v-slot:content>
+        <FormInput placeholder="Location name" v-model:value="locationName" />
+        <Dropdown placeholder="Space unit" :items="unitList" v-model:selected="selectedUnit" position="top" />
+      </template>
+      <template v-slot:footer>
+        <Button theme="submit" @click="saveLocationClicked" :loading="savingLocation">Save</Button>
+        <Button theme="danger" @click="showAddLocationPopup = false">Cancel</Button>
+      </template>
+    </Popup>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import { useStore } from 'vuex';
+import { wait } from '../../js/helper';
+
+const store = useStore();
 
 //#region Data
-const username = ref('');
+const allLocations = ref([]); // The spaces that are available for the user
+const locationLoading = ref(false); // Used to show the loading when loading the location from DB
+const showAddLocationPopup = ref(false); // Use to show the popup to add location
+const locationName = ref(''); // Used when adding a new location
+const selectedUnit = ref({ value: 'Square Feet (sqft)', acronym: 'sqft' }); // The unit when adding new location, default to sqft
+const unitList = ref([]); // The list of availale units
+const savingLocation = ref(false); // To show loading when saving the location
 //#endregion Data
 
+//#region Methods
+const locationDetailsClicked = (ind) => {
+  // Get the location based on the index
+  let location = allLocations.value[ind];
+  // Assign the location to the store
+  store.state.currentLocation = location;
+}
+const addLocationClicked = () => { // When adding a new location
+  // Reset the values to default
+  locationName.value = '';
+  selectedUnit.value = { value: 'Square Feet (sqft)', acronym: 'sqft' };
+
+  // To show the popup
+  showAddLocationPopup.value = true;
+}
+const saveLocationClicked = async () => { // When a new location is saved
+  // Simulate saving
+  savingLocation.value = true;
+  await wait(1000);
+  savingLocation.value = false;
+
+  // Insert to store [Call Backend in future]
+  store.commit('insertLocation', {
+    location: locationName.value,
+    unit: selectedUnit.value.acronym,
+    description: []
+  });
+  showAddLocationPopup.value = false;
+
+  // Get from store [Call backend]
+  allLocations.value = [];
+  locationLoading.value = true;
+  await wait(1000);
+  locationLoading.value = false;
+  allLocations.value = store.state.allLocation;
+}
+//#endregion Methods
+
 //#region Lifecycle
-onMounted(() => {
-  username.value = localStorage.getItem('user');
+onMounted(async () => {
+  unitList.value = store.state.unitListing;
+
+  locationLoading.value = true;
+  await wait(1000);
+  locationLoading.value = false;
+  allLocations.value = store.state.allLocation;
 });
 //#endregion Lifecycle
 </script>
 
 <style scoped>
-
+.home-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.add-space-button {
+  width: fit-content;
+  align-self: flex-end;
+}
+.space-container {
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  overflow: auto;
+  margin-top: 10px;
+}
+.space-details {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+.space-details:not(:first-child) {
+  margin-top: 10px;
+}
+.space-title {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+.space-description-container {
+  align-items: center;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
 </style>
