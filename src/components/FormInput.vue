644a143f85c14e8a3069b2cd53ce5832702b4e2e
Muhammad Azizi <azizi@hotmail.com.my>
2024-08-08 10:19:20 +0800

    Added error message that can be sent via the parent

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="input-main">
 		<div class="input-label">{{ props.placeholder }} <span v-if="isRequired" class="error-text">**</span></div>
-		<input class="input-box" type="text" v-model="inputValue" @input="userInput">
-		<!-- <div>Error message here</div> -->
+		<input :class="[{ 'input-box-error': errorMessage }, 'input-box']" type="text" v-model="inputValue" @input="userInput">
+		<div v-if="errorMessage" class="error-text">{{ errorMessage }}</div>
 	</div>
 </template>
 
@@ -13,9 +13,11 @@ const props = defineProps({
 	placeholder: { type: String, default: 'Label' }, // Use the name the label/placeholder
 	value: String, // Use for the value and it is binding to inputValue
 	isRequired: Boolean, // Used to indicate that this input is required
+	errorMessage: String, // Use to show the error message
 });
 const emit = defineEmits([
-	'update:value'
+	'update:value',
+	'update:errorMessage',
 ])
 
 //#region Data
@@ -24,6 +26,9 @@ const inputValue = ref(''); // Private value binded to the input element
 
 //#region Method
 const userInput = () => {
+	// Clear the error message from the parent component
+	emit('update:errorMessage', '');
+	// Updating the value back to the parent component
 	emit('update:value', inputValue.value);
 }
 //#endregion Method
@@ -55,6 +60,9 @@ watch(() => props.value, (val) => {
   background-color: rgba(211,211,211,0.2);
   border: 2px solid rgba(211,211,211,0.2);
   border-radius: 5px;
+}
+.input-box-error {
+	border: 2px solid indianred;
 }
 .input-box:focus {
   border: 2px solid gray;
