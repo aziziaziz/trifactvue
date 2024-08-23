1b556738493d405e9627400b79af0782b2ae8691
Muhammad Azizi <azizi@hotmail.com.my>
2024-08-24 01:04:19 +0800

    Update to match the json from backend and use save and share instead

--- src/views/phase1/BuildingSelection.vue
+++ src/views/phase1/BuildingSelection.vue
@@ -2,8 +2,8 @@
   <Loader v-if="loadingCriteria" text="Loading Criteria" />
   <div v-else class="building-selection-main">
     <div class="button-section">
-      <Button class="save-button" @click="shareClicked">Share</Button>
-      <Button class="save-button" theme="submit">Save</Button>
+      <Button class="save-button" @click="saveAndShareClicked">Save & Share</Button>
+      <Button class="save-button" theme="submit" @click="saveClicked">Save</Button>
     </div>
     <table>
       <tr>
@@ -11,15 +11,15 @@
         <th>Preference</th>
         <th>Weighting (1-10)</th>
       </tr>
-      <tr v-for="(crit,critInd) in criteria" :key="critInd">
+      <tr v-for="(crit,critInd) in currentBenchmark" :key="critInd">
         <td>
           <div class="criteria-section">
-            <div>{{ crit.criteria }}</div>
+            <div>{{ crit.description }}</div>
             <input type="text" placeholder="Remarks" v-model="crit.remarks">
           </div>
         </td>
-        <td><input type="text" v-model="crit.preference"></td>
-        <td><input type="text" v-model="crit.weighting"></td>
+        <td><input type="text" v-model="crit.user_preference"></td>
+        <td><input type="text" v-model="crit.weightage"></td>
       </tr>
     </table>
   </div>
@@ -46,9 +46,6 @@
           <div>Hours</div>
           <Button theme="submit" @click="extendTimeClicked">Extend</Button>
         </div>
-        <div class="extend-share-id-section">
-          <Button theme="submit" @click="updateShareDetailsClicked">Update Share Details</Button>
-        </div>
       </div>
       <div v-if="!loadingShareId" class="share-link">
         <div class="link">{{ shareLink }}</div>
@@ -63,14 +60,15 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
-import { showNoti } from '../../js/helper';
+import { showNoti, compareData } from '../../js/helper';
 import { useStore } from 'vuex';
 import { get, post, put } from '../../js/apiCall';
 
 const store = useStore();
 
 //#region Data
-const criteria = ref([]); // The list of criteria for the user building selection
+const currentBenchmark = ref([]); // The list of criteria for the user building selection
+const originalBenchmark = ref([]); // The original set of criteria from the DB for the client
 const loadingCriteria = ref(false); // Set to true whenn loading the criteria from DB
 const showSharePopup = ref(false); // To show the share popup
 const loadingShareId = ref(false); // When generating the share id
@@ -95,10 +93,13 @@ const shareClicked = async () => {
   if (previousShare.length > 0) { // If the share id has been generated
     linkSharedDetails.value = previousShare[0];
     shareLink.value = previousShare[0].share_id;
+
+    // Update the json details
+    await updateShareDetailsClicked();
   } else { // Generate a new share id
     // Generating the share link from the backend
     let url = `FormShare/GenerateShareId?client=${store.state.currentClient.client_uid}&page=BuildingSelection&user=${localStorage.getItem('user')}&expire=0.1`;
-    let shareId = await post(url, criteria.value);
+    let shareId = await post(url, currentBenchmark.value);
     shareLink.value = shareId;
   }
 
@@ -129,9 +130,29 @@ const updateShareDetailsClicked = async () => {
   // Update the json for the particular share id
   loadingShareId.value = true;
 
-  await put(`FormShare/UpdateJson?id=${shareLink.value}`, criteria.value);
+  await put(`FormShare/UpdateJson?id=${shareLink.value}`, currentBenchmark.value);
 
   loadingShareId.value = false;
+}
+const saveClicked = async () => {
+  // Get the data to post with action
+  let benchmarkToPost = compareData(originalBenchmark, currentBenchmark, 'benchmark_uid');
+  benchmarkToPost = benchmarkToPost.map(b => {
+    let obj = JSON.parse(JSON.stringify(b));
+    obj.weightage = obj.weightage ? Number(obj.weightage) : 0;
+    return obj;
+  });
+
+  // Posting the data
+  await put('BuildingBenchmark/UpdateClientBuildingBenchmark', benchmarkToPost);
+}
+const saveAndShareClicked = async () => {
+  // Show the popup
+  showSharePopup.value = true;
+
+  // Save the data first, then generate the share link
+  await saveClicked();
+  await shareClicked();
 }
 //#endregion Methods
 
@@ -139,23 +160,38 @@ const updateShareDetailsClicked = async () => {
 onMounted(async () => {
   // The list of criteria, call from DB once backend done
   loadingCriteria.value = true;
-  let benchmark = [];
 
   // Getting the benchmark for the user
-  benchmark = await get(`BuildingBenchmark/GetAllBuildingBenchmarkByClientUid?client_uid=${store.state.currentClient.client_uid}`);
+  currentBenchmark.value = await get(`BuildingBenchmark/GetAllBuildingBenchmarkByClientUid?client_uid=${store.state.currentClient.client_uid}`);
   // If user benchmark has not been saved, then get all the benchmark
-  if (benchmark.length == 0) {
+  if (currentBenchmark.value.length == 0) {
     // Get all the benchmark
-    benchmark = await get('BuildingBenchmark/GetAllBuildingBenchmark');
+    currentBenchmark.value = await get('BuildingBenchmark/GetAllBuildingBenchmark');
+    // Mapping the details and setting the remarks
+    currentBenchmark.value = currentBenchmark.value.map(b => ({
+      client_uid: store.state.currentClient.client_uid,
+      benchmark_uid: b.benchmark_uid,
+      unit: store.state.currentClient.sqm_sqft,
+      description: b.benchmark_description,
+      user_preference: '',
+      weightage: '',
+      remarks: ''
+    }));
+    originalBenchmark.value = [];
+  } else {
+    // Mapping the details and setting the remarks
+    currentBenchmark.value = currentBenchmark.value.map(b => ({
+      client_uid: b.client_uid,
+      benchmark_uid: b.benchmark_uid,
+      unit: b.unit,
+      description: b.description,
+      user_preference: b.user_preference,
+      weightage: b.weightage == 0 ? '' : b.weightage,
+      remarks: ''
+    }));
+    originalBenchmark.value = JSON.parse(JSON.stringify(currentBenchmark.value));
   }
 
-  // Mapping the benchmark details to the criteria object
-  criteria.value = benchmark.map(b => ({
-    criteria: b.benchmark_description,
-    preference: '',
-    weighting: '',
-    remarks: ''
-  }));
   loadingCriteria.value = false;
 });
 //#endregion Lifecycle
