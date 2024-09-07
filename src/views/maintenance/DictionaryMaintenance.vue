11a4258e2b65b1b90393ec9a0e77a5b01ff588bd
Muhammad Azizi <azizi@hotmail.com.my>
2024-09-07 13:35:59 +0800

    Getting all the dictionary listing from the DB

--- src/views/maintenance/DictionaryMaintenance.vue
+++ src/views/maintenance/DictionaryMaintenance.vue
@@ -1,112 +1,117 @@
-<!-- FIND [TOCHANGE] AND CHANGE ACCORDINGLY -->
-
 <template>
-  <div>Please select the dictionary that you would like to add, edit or remove.</div>
-  <!-- [TOCHANGE] Using dropdown -->
-  <select v-model="selectedDictionary">
-    <option value="" disabled selected>Select Dictionary</option>
-    <option v-for="(d,dInd) in dictionaryListing" :key="dInd" :value="d.value">{{ d.value }}</option>
-  </select>
-  <!-- [TOCHANGE] Using dropdown -->
+  <Loader v-if="allDictionaryLoading" text="Loading All Dictionaries" />
+  <div v-else class="dictionary-main">
+    <Dropdown :items="dictionaryListing" placeholder="Please select the dictionary that you would like to add, edit or remove." v-model:selected="selectedDictionary" />
 
-  <div v-if="dictionaryLoading">Loading</div>
-  <div v-else-if="dictionaryDetails.length > 0" class="details-main-container">
-    <Button theme="submit" class="save-dictionary-button" @click="saveDictionaryClicked">Save {{ selectedDictionary }}</Button>
-    <div v-for="(det, detInd) in dictionaryDetails" :key="detInd" class="details-container">
-      <div class="details-key">{{ det.key }}</div>
-      <Button theme="danger" @click="deleteItemClicked(detInd)">Delete</Button>
-    </div>
-    
-    <div class="details-container">
-      <!-- [TOCHANGE] Using input -->
-      <input type="text" v-model="newDetails">
-      <!-- [TOCHANGE] Using input -->
-      <Button theme="submit" @click="addNewDetailsClicked">Add</Button>
+    <Loader v-if="dictionaryLoading" text="Loading Dictionary Details" />
+    <div v-else-if="dictionaryDetails.length > 0" class="details-main-container">
+      <Button theme="submit" class="save-dictionary-button" @click="saveDictionaryClicked">Save {{ selectedDictionary }}</Button>
+      <div v-for="(det, detInd) in dictionaryDetails" :key="detInd" class="details-container">
+        <div class="details-key">{{ det.key }}</div>
+        <Button theme="danger" @click="deleteItemClicked(detInd)">Delete</Button>
+      </div>
+      
+      <div class="details-container">
+        <!-- [TOCHANGE] Using input -->
+        <input type="text" v-model="newDetails">
+        <!-- [TOCHANGE] Using input -->
+        <Button theme="submit" @click="addNewDetailsClicked">Add</Button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
+import { get } from '../../js/apiCall';
+import { wait } from '../../js/helper';
 
-  //#region Data
-  const dictionaryListing = ref([ // The list of the dictionary that user able to update [TOCHANGE] Using store
-    { value: 'Space Requirement' },
-    { value: 'Project Milestone' },
-    { value: 'Building Selection' }
-  ]);
-  const selectedDictionary = ref(''); // The dictionary that has been selected
-  const dictionaryLoading = ref(false); // When loading the dictionary from the DB
-  const dictionaryDetails = ref([]); // The details of the dictionary from the DB
-  const newDetails = ref(''); // The new details text that will be added
-  //#endregion Data
+//#region Data
+const allDictionaryLoading = ref(false); // When loading all the dictionaries
+const dictionaryListing = ref([]); // The list of available dictionary
+const selectedDictionary = ref(null); // The dictionary that has been selected
+const dictionaryLoading = ref(false); // When loading the dictionary from the DB
+const dictionaryDetails = ref([]); // The details of the dictionary from the DB
+const newDetails = ref(''); // The new details text that will be added
+//#endregion Data
 
-  //#region Methods
-  const loadDictionary = async () => {
-    // Loading the dictionary from the DB based on the selected dictionary
-    dictionaryLoading.value = true;
-    await wait(500);
-    // [TOCHANGE] Getting the details from DB
-    dictionaryDetails.value = [
-      { key: `${selectedDictionary.value} 1` },
-      { key: `${selectedDictionary.value} 2` },
-      { key: `${selectedDictionary.value} 3` },
-      { key: `${selectedDictionary.value} 4` },
-      { key: `${selectedDictionary.value} 5` }
-    ];
-    dictionaryLoading.value = false;
-  }
-  const addNewDetailsClicked = () => {
-    // Checking if the new details is not filled in
-    if (newDetails.value) {
-      // Checking if the details are already in the list
-      var hasIndex = dictionaryDetails.value.findIndex(det => det.key == newDetails.value);
+//#region Methods
+const loadDictionary = async () => {
+  // Loading the dictionary from the DB based on the selected dictionary
+  dictionaryLoading.value = true;
+  await wait(500);
+  // [TOCHANGE] Getting the details from DB
+  dictionaryDetails.value = [
+    { key: `${selectedDictionary.value} 1` },
+    { key: `${selectedDictionary.value} 2` },
+    { key: `${selectedDictionary.value} 3` },
+    { key: `${selectedDictionary.value} 4` },
+    { key: `${selectedDictionary.value} 5` }
+  ];
+  dictionaryLoading.value = false;
+}
+const addNewDetailsClicked = () => {
+  // Checking if the new details is not filled in
+  if (newDetails.value) {
+    // Checking if the details are already in the list
+    var hasIndex = dictionaryDetails.value.findIndex(det => det.key == newDetails.value);
 
-      if (hasIndex >= 0) { // Found within the list
-        // [TOCHANGE] Using noti
-        alert(`${newDetails.value} is already in the list`);
-      } else { // Not found within the list
-        // [TOCHANGE] This to change to properly push
-        dictionaryDetails.value.push({ key: newDetails.value });
-  
-        // Clearing the new details
-        newDetails.value = '';
-      }
-    } else {
+    if (hasIndex >= 0) { // Found within the list
       // [TOCHANGE] Using noti
-      alert('Please fill in the new details');
+      alert(`${newDetails.value} is already in the list`);
+    } else { // Not found within the list
+      // [TOCHANGE] This to change to properly push
+      dictionaryDetails.value.push({ key: newDetails.value });
+
+      // Clearing the new details
+      newDetails.value = '';
     }
+  } else {
+    // [TOCHANGE] Using noti
+    alert('Please fill in the new details');
   }
-  const saveDictionaryClicked = () => {
-    // Get the compared values from helper
-    // [TOCHANGE] To change and call the API to save
-  }
-  const deleteItemClicked = (ind) => {
-    // Remove from the dictionary details
-    dictionaryDetails.value.splice(ind, 1);
-  }
-  // [TOCHANGE] To remove as this is using wait method
-  const wait = (time) => {
-    let res = null;
-    let prom = new Promise(r => res = r);
+}
+const saveDictionaryClicked = () => {
+  // Get the compared values from helper
+  // [TOCHANGE] To change and call the API to save
+}
+const deleteItemClicked = (ind) => {
+  // Remove from the dictionary details
+  dictionaryDetails.value.splice(ind, 1);
+}
+//#endregion Methods
 
-    setTimeout(() => res(), time);
+//#region Lifecycle
+onMounted(async () => {
+  // Get all the available dictionaries
+  allDictionaryLoading.value = true;
+  let dictionaries = await get('Dictionary/GetAllDictionaries');
 
-    return prom
+  // Reformat to have the value
+  dictionaryListing.value = dictionaries.map(d => {
+    d.value = d.table_Description;
+    return d;
+  });
+
+  allDictionaryLoading.value = false;
+})
+//#endregion Lifecycle
+
+//#region Watcher
+watch(selectedDictionary, (newVal) => {
+  if (newVal) {
+    loadDictionary();
   }
-  // [TOCHANGE] To remove as this is using wait method
-  //#endregion Methods
-
-  //#region Watcher
-  watch(selectedDictionary, (newVal) => {
-    if (newVal) {
-      loadDictionary();
-    }
-  })
-  //#endregion Watcher
+})
+//#endregion Watcher
 </script>
 
 <style scoped>
+.dictionary-main {
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
+}
 .details-main-container {
   display: flex;
   flex-direction: column;
