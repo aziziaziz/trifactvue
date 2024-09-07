f6b80252788f0ed6882fba08ee333651d1eb2861
Muhammad Azizi <azizi@hotmail.com.my>
2024-09-07 12:29:24 +0800

    Completed the update of the user

--- src/views/maintenance/UserMaintenance.vue
+++ src/views/maintenance/UserMaintenance.vue
@@ -10,24 +10,24 @@
         <div>{{ u.email }}</div>
       </div>
       <div class="user-buttons">
-        <Button theme="submit">Edit</Button>
+        <Button theme="submit" @click="editUserClicked(u)">Edit</Button>
         <Button v-if="(u.username != 'Master1' && u.username != 'Master2') && u.username != currentUser" theme="danger" @click="deleteUserClicked(u)">Delete</Button>
       </div>
     </div>
   </div>
 
   <Popup :show="showAddPopup">
-    <template v-slot:header>Add New User</template>
+    <template v-slot:header>{{ editingUser ? 'Edit' : 'Add New' }} User</template>
     <template v-slot:content>
       <Loader v-if="loadingRoles" text="Loading Roles" />
       <div v-else class="add-container">
-        <FormInput placeholder="Username" v-model:value="newUsername" />
-        <FormInput placeholder="Email" v-model:value="newEmail" />
+        <FormInput placeholder="Username" v-model:value="newUsername" :disabled="editingUser != null" />
+        <FormInput placeholder="Email" v-model:value="newEmail" :disabled="editingUser != null" />
         <Dropdown placeholder="Role" position="top" :items="roleListing" v-model:selected="selectedRole" />
       </div>
     </template>
     <template v-slot:footer>
-      <Button theme="submit" @click="addNewUserClicked" :loading="addNewUserLoading">Add User</Button>
+      <Button theme="submit" @click="addNewUserClicked" :loading="addNewUserLoading">{{ editingUser ? 'Update' : 'Add' }} User</Button>
       <Button theme="danger" @click="showAddPopup = false">Cancel</Button>
     </template>
   </Popup>
@@ -35,7 +35,7 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
-import { get, httpDelete, post } from '../../js/apiCall';
+import { get, httpDelete, post, put } from '../../js/apiCall';
 import { question, showNoti } from '../../js/helper';
 
 //#region Data
@@ -51,23 +51,18 @@ const newUsername = ref(''); // The username when adding the user
 const newEmail = ref(''); // The email when adding the user
 const addNewUserLoading = ref(false); // Loading when saving the new user
 const deleteUserText = ref(''); // The text to show when deleting a user
+const editingUser = ref(null); // The user object that is being edit
 //#endregion Data
 
 //#region Methods
 const addUserClicked = async () => {
-  // Setting back to default values
-  selectedRole.value = null;
-  newUsername.value = '';
-  newEmail.value = '';
+  setPopupDefaultValues();
 
   // To show the popup
   showAddPopup.value = true;
 
   // Getting the roles from DB
-  loadingRoles.value = true;
-  roleListing.value = await get(`Role/GetAllowedRoles?roleId=${localStorage.getItem('role')}`);
-  roleListing.value.forEach(r => r.value = r.role_desc);
-  loadingRoles.value = false;
+  getAllRoles();
 }
 const getAllUsers = async () => {
   // Clear the current users first
@@ -97,33 +92,64 @@ const addNewUserClicked = async () => {
     return;
   }
 
-  // Asking if the email is correct
-  let confirmEmail = await question('Confirm User Email', `Do you confirm that the user email (${newEmail.value}) is correct?\nUser will receive an email with the password for first time login.\nWrong email will cause the user unable to login.`, 'Confirm', 'Cancel');
-
-  // Checking if user confirms the email
-  if (confirmEmail == 'Confirm') {
-    // Inserting the new user to DB
+  if (editingUser.value) { // For edit
+    // Generate the update to update
+    let userToUpdate = {
+      user_id: editingUser.value.user_id,
+      username: editingUser.value.username,
+      email: editingUser.value.email,
+      role: selectedRole.value.role_id,
+      deleted: 'N',
+    };
+    
+    // Update the user
     addNewUserLoading.value = true;
-    let addUserResult = await post('User/Register', {
-      username: newUsername.value,
-      email: newEmail.value,
-      role: selectedRole.value.role_id
-    });
+    let updateUserResult = await put('User/UpdateUser', userToUpdate);
     addNewUserLoading.value = false;
 
-    // Check if the insertion of new user success
-    if (addUserResult) {
+    // Check if update successful
+    if (updateUserResult) {
       // Close the popup
       showAddPopup.value = false;
 
       // Show success noti
-      showNoti('Successfully created a new user and email has been sent to the user containing their new password.', 'success');
+      showNoti(`Successfully updated ${editingUser.value.username}`, 'success');
 
-      // Call back the load user api so that the list gets updated
-      await getAllUsers();
+      // Reload all the users
+      await getAllUsers()
     } else {
       // Show error noti
-      showNoti('There was an error while creating the user. Please try again.', 'error');
+      showNoti('Failed to update user. Please try again.', 'error');
+    }
+  } else { // For new
+    // Asking if the email is correct
+    let confirmEmail = await question('Confirm User Email', `Do you confirm that the user email (${newEmail.value}) is correct?\nUser will receive an email with the password for first time login.\nWrong email will cause the user unable to login.`, 'Confirm', 'Cancel');
+
+    // Checking if user confirms the email
+    if (confirmEmail == 'Confirm') {
+      // Inserting the new user to DB
+      addNewUserLoading.value = true;
+      let addUserResult = await post('User/Register', {
+        username: newUsername.value,
+        email: newEmail.value,
+        role: selectedRole.value.role_id
+      });
+      addNewUserLoading.value = false;
+
+      // Check if the insertion of new user success
+      if (addUserResult) {
+        // Close the popup
+        showAddPopup.value = false;
+
+        // Show success noti
+        showNoti('Successfully created a new user and email has been sent to the user containing their new password.', 'success');
+
+        // Call back the load user api so that the list gets updated
+        await getAllUsers();
+      } else {
+        // Show error noti
+        showNoti('There was an error while creating the user. Please try again.', 'error');
+      }
     }
   }
 }
@@ -151,6 +177,38 @@ const deleteUserClicked = async (user) => {
     // Reload all the users again
     await getAllUsers();
   }
+}
+const editUserClicked = async (user) => {
+  // Setting back to default values
+  setPopupDefaultValues();
+
+  // Getting all the roles
+  await getAllRoles();
+
+  // Set the editing user
+  editingUser.value = user;
+
+  // Set to the current editing user
+  newUsername.value = editingUser.value.username;
+  newEmail.value = editingUser.value.email;
+  selectedRole.value = roleListing.value.find(r => r.role_id == editingUser.value.role);
+
+  // Show the popup
+  showAddPopup.value = true;
+}
+const setPopupDefaultValues = () => {
+  // Setting back to default values
+  selectedRole.value = null;
+  newUsername.value = '';
+  newEmail.value = '';
+  editingUser.value = null;
+}
+const getAllRoles = async () => {
+  // Getting all the roles
+  loadingRoles.value = true;
+  roleListing.value = await get(`Role/GetAllowedRoles?roleId=${localStorage.getItem('role')}`);
+  roleListing.value.forEach(r => r.value = r.role_desc);
+  loadingRoles.value = false;
 }
 //#endregion Methods
 
