0bba6b600d04c7767615b54f15cc9a5864df6ae5
Muhammad Azizi <azizi@hotmail.com.my>
2024-10-05 14:54:57 +0800

    Calling API and include the sample pie chart

--- src/views/phase1/CostBreakdown.vue
+++ src/views/phase1/CostBreakdown.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <Loader v-if="loadingCostBreakdown" text="Loading Cost Breakdown" />
+  <div v-else>
     <table>
       <tr>
         <th>Description</th>
@@ -7,30 +8,56 @@
         <th>Variation Order</th>
         <th>Total</th>
       </tr>
-      <tr>
-        <td>Preliminary</td>
-        <td>100</td>
-        <td></td>
-        <td>100</td>
-      </tr>
-      <tr>
-        <td>Fit out Work</td>
-        <td>400</td>
-        <td>29</td>
-        <td>429</td>
-      </tr>
-      <tr>
-        <td>MEP Work</td>
-        <td>1200</td>
-        <td>300</td>
-        <td>1500</td>
+      <tr v-for="(cb,cbInd) in costBreakdownListing" :key="cbInd">
+        <td>{{ cb.cost_breakdown_name }}</td>
+        <td>{{ formatNumber(cb.ori_sub_total_amnt) }}</td>
+        <td>{{ formatNumber(cb.var_sub_total_amnt) }}</td>
+        <td>{{ formatNumber(Number(cb.ori_sub_total_amnt) + Number(cb.var_sub_total_amnt)) }}</td>
       </tr>
     </table>
+
+    <div class="chart-container">
+      <div class="top-chart-container">
+        <div class="chart-box">
+          <PieChart />
+        </div>
+        <div class="chart-box">
+          <PieChart />
+        </div>
+      </div>
+      <div class="chart-box">
+        <PieChart />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {} from 'vue';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { get } from '../../js/apiCall';
+import { formatNumber } from '../../js/helper';
+
+const store = useStore();
+const router = useRouter();
+
+//#region Data
+const costBreakdownListing = ref([]); // The listing of the cost breakdown
+const loadingCostBreakdown = ref(false); // To used when loading the cost breakdown
+//#endregion Data
+
+//#region Lifecycle
+onMounted(async () => {
+  if (store.state.currentClient) {
+    // Getting the details for the currentclient
+    costBreakdownListing.value = await get(`Budget/GetCostBreakdown?Client_uid=${store.state.currentClient.client_uid}`);
+  } else {
+    // Push back to home page to choose client
+    router.push('/Home?choose');
+  }
+});
+//#endregion Lifecycle
 </script>
 
 <style scoped>
@@ -44,5 +71,20 @@ table tr td, table tr th {
 }
 td:not(:first-child) {
   text-align: center;
+}
+.chart-container {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+}
+.top-chart-container {
+  display: flex;
+  width: 100%;
+  column-gap: 10px;
+}
+.chart-box {
+  width: 50%;
 }
 </style>
